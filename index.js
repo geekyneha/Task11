ac64de8ba2ff6e15add9adef5fbf5d5ba7058a24
Neha <nehatualea@gmail.com>
2024-10-22 19:07:38 +0530

    odd even

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let num=10;
+let b=13;
+let c=2;
+let d=7;
+
+
+if(num%2==0){
+    console.log(`The number ${num} is even.`)
+}
+else{
+    console.log(`The number ${num} is odd.`)
+}
+
+if(b%2==0){
+    console.log(`The number ${b} is even.`)
+}
+else{
+    console.log(`The number ${b} is odd.`)
+}
+if(c%2==0){
+    console.log(`The number ${c} is even.`)
+}
+else{
+    console.log(`The number ${c} is odd.`)
+}
+if(d%2==0){
+    console.log(`The number ${d} is even.`)
+}
+else{
+    console.log(`The number ${d} is odd.`)
+}
